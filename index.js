81440f75515babddb53a67ee4265f38ebc437620
Damian <damianczapiewski@gmail.com>
2025-03-26 15:37:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,20 +3,20 @@
 Math.interpolate = lagrangeInterpolation;
 
 function lagrangeInterpolation(points) {
-	return function (x) {
-		return points.reduce((acc,[,yi],i) => {
-			return acc + yi * _lg(i,x);
-		},0);
-	};
+  return function (x) {
+    return points.reduce((acc,[,yi],i) => {
+      return acc + yi * _lg(i,x);
+    },0);
+  };
 	
-	function _lg(i,x) {
-		const [xi] = points[i];
-		return points.reduce((acc,[xj],j) => {
-			return acc * (
-				((x-xj)/(xi-xj))**(1-(j===i))
-			);
-		},1);
-	}
+  function _lg(i,x) {
+    const [xi] = points[i];
+    return points.reduce((acc,[xj],j) => {
+      return acc * (
+        ((x-xj)/(xi-xj))**(1-(j===i))
+      );
+    },1);
+  }
 }
 
 })()
